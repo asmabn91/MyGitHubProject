1250e3d82e4bc7076bfdf38f5d73d6dfd15eb4cf
asma <asma@portakal.net>
2023-01-30 18:45:41 -0800

    new added class

---
+++ src/Day1GitHub/ThirdGitHubClass.java
@@ -0,0 +1,8 @@
+package Day1GitHub;
+
+public class ThirdGitHubClass {
+    public static void main(String[] args) {
+        System.out.println("Hello to my world!");
+        System.out.println("you are in asmafeature");
+    }
+}
