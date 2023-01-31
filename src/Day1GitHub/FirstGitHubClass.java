a527bafa84f597d2f74f34fd45e7241a0caaad4e
Asma Altuner <123770514+asmabn91@users.noreply.github.com>
2023-01-30 18:07:00 -0800

    Update FirstGitHubClass.java

--- src/Day1GitHub/FirstGitHubClass.java
+++ src/Day1GitHub/FirstGitHubClass.java
@@ -2,7 +2,7 @@ package Day1GitHub;
 
 public class FirstGitHubClass {
     public static void main(String[] args) {
-        System.out.println("hey GitHub ! ");
+        System.out.println("hello GitHub ! ");
         System.out.println(1+1);
 
     }
