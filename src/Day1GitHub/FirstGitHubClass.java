b228cd3ac58f34bd6946c968c6de9d5057570d02
asma <asma@portakal.net>
2023-01-30 17:25:37 -0800

    i added some codes

---
+++ src/Day1GitHub/FirstGitHubClass.java
@@ -0,0 +1,9 @@
+package Day1GitHub;
+
+public class FirstGitHubClass {
+    public static void main(String[] args) {
+        System.out.println("hey GitHub ! ");
+        System.out.println(1+1);
+
+    }
+}
